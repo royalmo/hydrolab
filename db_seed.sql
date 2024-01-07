ae38036d1faf82252a383c8b7c113ff65cb0b6d9
royalmo <royalmo6@gmail.com>
2024-01-07 18:25:51 +0100

    Added missing comma

--- db_seed.sql
+++ db_seed.sql
@@ -6,6 +6,6 @@ INSERT INTO user VALUES
 ;
 
 INSERT INTO sensor (eui, name) VALUES
-    ('eui-70b3d57ed8001cab', 'HydroLab OTAA 1')
+    ('eui-70b3d57ed8001cab', 'HydroLab OTAA 1'),
     ('eui-70b3d57ed80024ae', 'HydroLab OTAA 2')
 ;
